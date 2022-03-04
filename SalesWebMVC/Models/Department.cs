3710f0a80c8de726da7fe6fd19f35cc332210054
MarlyssonMendes <marlysson.oliveira@hotmail.com>
2022-03-04 17:00:09 -0300

    first files genereted removed

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void addSeller()
+        {
+
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return 0.0;
+        }
+    }
+}
